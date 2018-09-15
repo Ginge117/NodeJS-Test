8831502715ebbf2018188a59ab1ef0464e012568
Nathan Brereton-Ferns <Nathan-bf@live.co.uk>
2018-09-15 18:31:19 +0000

    Playing with web servers

--- Test1/startup.js
+++ Test1/startup.js
@@ -1,1 +1,25 @@
 console.log("This is a test file");
+var http = require("http");
+var fs = require("fs");
+http.createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    var file = fs.createReadStream("/home/ec2-user/environment/NodeJS-Test/Test1/startup.js");
+    file.pipe(res);
+    file.on("end", function() {
+        res.close;
+    });
+}).listen(process.env.PORT, process.env.IP);
+console.log("Server Started!");
+console.log("Location :" + process.env.IP + ":" + process.env.PORT);
+
+
+function readFile(filePath, callback) {
+    fs.readFile(filePath, function(err, contents) {
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
+            callback(null, contents);
+        }
+    });
+}
---
+++ Test1/test.js
@@ -0,0 +1,26 @@
+function doAThing(val, callback) {
+    var start = new Date();
+    var error;
+    var result;
+    if (val % 2 == 0) {
+        result = val * 2;
+    } else {
+        error = new Error("An error occured");
+    }
+    var timer = Math.random() * 100;
+    setTimeout(callback, timer, error, result, ((new Date().getMilliseconds() + timer) - start.getMilliseconds()).toFixed(0));
+}
+
+
+function handleResults(error, value, time) {
+    if (error) {
+        console.log(error + " (" + time + "ms)");
+    } else {
+        console.log("Value: " + value + " (" + time + "ms)");
+    }
+}
+
+doAThing(1, handleResults);
+doAThing(2, handleResults);
+doAThing(3, handleResults);
+doAThing(4, handleResults);
